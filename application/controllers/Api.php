c640611d7989809d8cadb1f0bdc70e7e1f1715f5
Angelos Chalaris <chalarangelo@gmail.com>
2017-06-24 10:26:15 +0300

    Added authorization
    An API endpoint pseudo-resource is the login one, which is used to exchange credentials for a jwt token. That token can then be used to identify the user, as required, so that a sessionless RESTful API can do its business. Relies heavily upon the JWT code.

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -21,6 +21,41 @@ class Api extends CI_Controller {
     echo(json_encode(array('message'=>'No resource specified.')));
   }
 
+  public function login(){
+    header('Content-Type: application/json');
+    if($this->input->method(true) != 'POST'){
+      echo json_encode(array("message:"=>"Use the HTTP POST method to login to the system."));
+      retun;
+    }
+    else {
+      $login_fields = "`username`, `password`, `user_id` AS `id`";
+      $data = $this->readResourceElement(
+          $this::$users["table"], $login_fields, $this::$users["read_key"],
+          $_POST["username"]
+      );
+      if($data == false){
+        echo(json_encode(array('message'=>'No user with this username.')));
+        return;
+      }
+      else {
+        if(password_verify(hash("sha512", $_POST["password"], true), $data[0]["password"])){
+          $token = array(
+            "username" => $_POST["username"],
+            "id" => $data[0]["id"],
+            "iat" => date_timestamp_get(date_create()),
+            "iss" => "pyramids.social"
+          );
+          $config = parse_ini_file(__DIR__.'/../../config.ini');
+          echo JWT::encode($token, $config["secret"]);
+        }
+        else {
+          echo(json_encode(array('message'=>'Password does not match.')));
+          return;
+        }
+      }
+    }
+  }
+
   /*
    * Method for getting `user` resources.
    * Parameters:
@@ -33,7 +68,9 @@ class Api extends CI_Controller {
     if($this->input->method(true) == 'POST'){
       echo json_encode($this->createResourceElement(
         $this::$users["table"], $this::$users["create_fields"], $this::$users["create_types"],
-        [$_POST["username"], $_POST["password"], $_POST["email"]]
+        [$_POST["username"],
+        password_hash(hash("sha512", $_POST["password"], true), PASSWORD_DEFAULT),
+        $_POST["email"]]
       ));
       return;
     }
